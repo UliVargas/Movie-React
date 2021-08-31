6f41be050f6dddb11dda5f061cd748b867f82d11
UliVargas <uli.vargas02@gmail.com>
2021-08-31 13:56:27 -0500

    Se agrega snipper de carga en la seccion de detalles de pelicula

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -2,29 +2,34 @@ import styles from './MovieDetails.module.css'
 import { Get } from '../utils/HTTPSClient'
 import {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
+import { Spinner } from '../components/Spinner';
 
 export function MovieDetails() {
     const { movieId } = useParams();
     const [Movie, setMovie] = useState([]);
+    const [Loading, setLoading] = useState(true);
 
     useEffect(() => {
+            setLoading(true)
         Get('/movie/' + movieId).then((data) => {
+            setLoading(false);
             setMovie(data)
-            console.log(data)
         })
         }, [movieId]);
-
+    if(Loading) {
+        return <Spinner />
+    }
     if(!Movie) {
         return null;
     }
-    console.log(Movie)
+    console.log(Movie.genres)
     const ImageUrl = 'https://image.tmdb.org/t/p/w500' + Movie.poster_path;
     return (
         <div className={styles.movieDetails}>
             <img  className={`${styles.col} ${styles.movieImage}`} src={ImageUrl} alt={Movie.title}/>
             <div className={`${styles.col} ${styles.movieTitle}`}>
                 <p className={styles.firstTitle}><strong>Title:</strong> {Movie.title}</p>
-                {/*<p><strong>Genres:</strong> {Movie.genres.map((genre) => genre.name).join(', ')}</p>*/}
+                <p><strong>Genres:</strong>{' '}</p>
                 <p><strong>Description:</strong> {Movie.overview}</p>
             </div>
         </div>
